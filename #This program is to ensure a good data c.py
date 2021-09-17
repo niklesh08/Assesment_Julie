836a52f7eb56d6ec79a0359647d3ccbb98cebaab
niklesh08 <niklesh.naidu@gmail.com>
2021-09-17 15:34:18 +1200

    Update #This program is to ensure a good data c.py

--- #This program is to ensure a good data c.py
+++ #This program is to ensure a good data c.py
@@ -9,3 +9,13 @@ from tkinter import *
 def quit () :
     main_window.destroy()
 
+#print details of all the customers
+def print_customer_details () :
+    #these are global variables that are used
+    global j_names, total_entries, name_count
+    name_count=0
+    #Create the column headings
+    Label(main_window, font=("Helvetica 10 bold"), text="Full Name") .grid(column=0, row=7)
+    Label(main_window, font=("Helvetica 10 bold"), text="Receipt Number") .grid(column=1, row=7)
+    Label(main_window, font=("Helvetica 10 bold"), text="Item") .grid(column=2, row=7)
+    Label(main_window, font=("Helvetica 10 bold"), text="Number of Item/s") .grid(column=3, row=7)
