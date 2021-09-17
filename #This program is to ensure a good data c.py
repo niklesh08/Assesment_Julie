47a09fb95120ea1a21b65135907b09c5458d2004
niklesh08 <niklesh.naidu@gmail.com>
2021-09-17 16:21:46 +1200

    Update #This program is to ensure a good data c.py

--- #This program is to ensure a good data c.py
+++ #This program is to ensure a good data c.py
@@ -117,10 +117,16 @@ def setup_buttons():
 
 
 #start the program running
-def main ():
+def main():
+    #these are the global variables that are used
+    global main_window
+    global customer_details, entry_name,entry_receipt,entry_item,entry_quantity, total_entries
+    #create empty list for camp details and empty variable for entries in the list
+    customer_details = []
+    total_entries = 0
     #create the GUI and start it up
-    main_window =Tk ()
-    setup_buttons ()
-    main_window.mainloop ()
-        
-main ()
+    main_window =Tk()
+    setup_buttons()
+    main_window.mainloop()
+    
+main()
