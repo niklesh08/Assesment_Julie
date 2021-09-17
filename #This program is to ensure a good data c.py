7f3975eabe6319aac4855d90356f1020798db95a
niklesh08 <niklesh.naidu@gmail.com>
2021-09-17 15:20:34 +1200

    Update #This program is to ensure a good data c.py

--- #This program is to ensure a good data c.py
+++ #This program is to ensure a good data c.py
@@ -1,3 +1,6 @@
 ##############################################################################
 #This program is to ensure a good data collection of Julie's Party Hire Store#
 ##############################################################################
+
+#import tkinter so we can make a GUI
+from tkinter import *
