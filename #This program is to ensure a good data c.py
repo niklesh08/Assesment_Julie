3223393972a7a4b2f901d90aca438e66f20782af
niklesh08 <niklesh.naidu@gmail.com>
2021-09-17 16:12:30 +1200

    Update #This program is to ensure a good data c.py

--- #This program is to ensure a good data c.py
+++ #This program is to ensure a good data c.py
@@ -38,26 +38,39 @@ def check_inputs ():
     Label(main_window, text="               ") .grid(column=2,row=2)
     Label(main_window, text="               ") .grid(column=2,row=3)
     #Check that Full Name is not blank, set error text if blank   
-    if len(entry_leader.get()) == 0 :
+    if len(entry_name.get()) == 0 :
         Label(main_window,fg="red", text="Required") .grid(column=2,row=0)
         input_check = 1
     #Check that Receipt Number is not blank, set error text if blank     
-    if len(entry_location.get()) == 0 :
+    if len(entry_receipt.get()) == 0 :
         Label(main_window,fg="red", text="Required") .grid(column=2,row=1)
         input_check = 1
     #Check that Item is not blank, set error text if blank     
-    if len(entry_location.get()) == 0 :
+    if len(entry_item.get()) == 0 :
         Label(main_window,fg="red", text="Required") .grid(column=2,row=2)
         input_check = 1
     #Check the number of item/s is not blank and between 1 and 500, set error text if blank  
-    if (entry_campers.get().isdigit()) : 
-        if  int(entry_campers.get()) < 1 or  int(entry_campers.get()) > 500:
+    if (entry_quantity.get().isdigit()) : 
+        if  int(entry_quantity.get()) < 1 or  int(entry_quantity.get()) > 500:
             Label(main_window,fg="red", text="1-500 only") .grid(column=2,row=2)
             input_check = 1
     else :
         Label(main_window,fg="red", text="1-500 only") .grid(column=2,row=2)
         input_check = 1      
     if input_check == 0 : append_name()
+
+#add the next customer to the list
+def append_name ():
+    #these are the global variables that are used
+    global customer_details, entry_name, entry_receipt, entry_item, entry_quantity, total_entries
+    #append each item to its own area of the list
+    customer_details.append([entry_name.get(),entry_receipt.get(),entry_item.get(),entry_quantity.get()])
+    #clear the boxes
+    entry_name.delete(0,'end')
+    entry_receipt.delete(0,'end')
+    entry_item.delete(0,'end')
+    entry_quantity.delete(0,'end')
+    total_entries +=  1
 
 #start the program running
 def main ():
